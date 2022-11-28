a53d332ddb66c5388251c4c671ea829c783a159b
PedroD <nevesdd01@outlook.com>
2022-11-27 21:44:59 -0500

    made more changes

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   const [name, setName] = useState([])
 
   useEffect(() => {
-    fetch('https://starter-name-generator-utr6.onrender.com/generate/')
+    fetch('https://name-generator-frontend-07uj.onrender.com/generate')
       .then((res) => res.json())
       .then((nme) => setName([nme]))
   }, [])
